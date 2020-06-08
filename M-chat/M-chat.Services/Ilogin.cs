2d00f8f16c882c4d24a7a01f514336e2711c17a3
raccpunk <66329111+raccpunk@users.noreply.github.com>
2020-06-07 23:29:13 -0500

    Initial commit

---
+++ M-chat/M-chat.Services/Ilogin.cs
@@ -0,0 +1,12 @@
+﻿using M_chat.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M_chat.Services
+{
+    public interface Ilogin
+    {
+        public IEnumerable<Secion> GetAll();
+    }
+}
---
+++ M-chat/M-chat/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using M_chat.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace M_chat.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        //public void OnGet()
+        //{
+        //    public class pageModel : IndexModel
+        //{
+        //    private readonly Ilogin secion;
+        //    public IndexModel(Ilogin secion)
+        //    {
+        //        this.secion = secion;
+        //    }
+            public void OnGet()
+            {
+            }
+        }
+        }
+    //}
+    
+    
+
